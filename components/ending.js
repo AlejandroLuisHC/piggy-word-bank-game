7fe2d4598f3257c73bd1f9ec384d3de4cb342e6f
AlejandroLHC <alejandrolhc@gmail.com>
2022-08-04 12:00:24 +0200

    Create working environment for 'ending'

---
+++ components/ending.js
@@ -0,0 +1,18 @@
+const
+    mainDisplay         = document.getElementById("mainDisplay"),
+    userAccess          = document.getElementById("userAccess"),
+    form                = document.getElementById("form"),
+    username            = document.getElementById("username"),
+    submitBtn           = document.getElementById("submitBtn"),
+    rankingDisplay      = document.getElementById("rankingDisplay"),
+    topRank             = document.getElementById("topRank"),
+    rank1               = document.getElementById("rank1"),
+    rank2               = document.getElementById("rank2"),
+    rank3               = document.getElementById("rank3"),
+    rank4               = document.getElementById("rank4"),
+    rank5               = document.getElementById("rank5");
+
+const papaFrita = document.createElement("h1");
+papaFrita.textContent = "You won!";
+papaFrita.style.color = "red";
+mainDisplay.appendChild(papaFrita);
