060101da900b258e059809f3746b102e619fff9c
AlejandroLHC <alejandrolhc@gmail.com>
2022-08-08 11:48:47 +0200

    Fix keyboard & Add piggy

--- components/game.js
+++ components/game.js
@@ -45,37 +45,45 @@ drawingWordDiv.appendChild(word);
 
 const keyboard = document.createElement('div');
 keyboard.className                    = "keyboard";
-keyboard.style.display                = "grid";
-keyboard.style.gridTemplateColumns    = "repeat(9, 1fr)";
-keyboard.style.rowGap                 = "20px";
+keyboard.style.display                = "flex";
+keyboard.style.flexDirection          = "column"
+keyboard.style.rowGap                 = "20px"
+keyboard.style.justifyContent         = "center"
+keyboard.style.alignItems             = "center"
 keyboard.style.width                  = "100%";
 
 keyboard.innerHTML = `
-    <button id="q" onclick="letterInput(this)">Q</button>
-    <button id="w" onclick="letterInput(this)">W</button>
-    <button id="e" onclick="letterInput(this)">E</button>
-    <button id="r" onclick="letterInput(this)">R</button>
-    <button id="t" onclick="letterInput(this)">T</button>
-    <button id="y" onclick="letterInput(this)">Y</button>
-    <button id="u" onclick="letterInput(this)">U</button>
-    <button id="i" onclick="letterInput(this)">I</button>
-    <button id="o" onclick="letterInput(this)">O</button>
-    <button id="p" onclick="letterInput(this)">P</button>
-    <button id="a" onclick="letterInput(this)">A</button>
-    <button id="s" onclick="letterInput(this)">S</button>
-    <button id="d" onclick="letterInput(this)">D</button>
-    <button id="f" onclick="letterInput(this)">F</button>
-    <button id="g" onclick="letterInput(this)">G</button>
-    <button id="h" onclick="letterInput(this)">H</button>
-    <button id="j" onclick="letterInput(this)">J</button>
-    <button id="k" onclick="letterInput(this)">K</button>
-    <button id="l" onclick="letterInput(this)">L</button>
-    <button id="z" onclick="letterInput(this)">Z</button>
-    <button id="x" onclick="letterInput(this)">X</button>
-    <button id="c" onclick="letterInput(this)">C</button>
-    <button id="v" onclick="letterInput(this)">V</button>
-    <button id="b" onclick="letterInput(this)">B</button>
-    <button id="n" onclick="letterInput(this)">N</button>
-    <button id="m" onclick="letterInput(this)">M</button>
+    <div style="display: flex; column-gap: 20px;">
+        <button id="q" onclick="letterInput(this)">Q</button>
+        <button id="w" onclick="letterInput(this)">W</button>
+        <button id="e" onclick="letterInput(this)">E</button>
+        <button id="r" onclick="letterInput(this)">R</button>
+        <button id="t" onclick="letterInput(this)">T</button>
+        <button id="y" onclick="letterInput(this)">Y</button>
+        <button id="u" onclick="letterInput(this)">U</button>
+        <button id="i" onclick="letterInput(this)">I</button>
+        <button id="o" onclick="letterInput(this)">O</button>
+        <button id="p" onclick="letterInput(this)">P</button>
+    </div>
+    <div style="display: flex; column-gap: 20px;">
+        <button id="a" onclick="letterInput(this)">A</button>
+        <button id="s" onclick="letterInput(this)">S</button>
+        <button id="d" onclick="letterInput(this)">D</button>
+        <button id="f" onclick="letterInput(this)">F</button>
+        <button id="g" onclick="letterInput(this)">G</button>
+        <button id="h" onclick="letterInput(this)">H</button>
+        <button id="j" onclick="letterInput(this)">J</button>
+        <button id="k" onclick="letterInput(this)">K</button>
+        <button id="l" onclick="letterInput(this)">L</button>
+    </div>
+    <div style="display: flex; column-gap: 20px;">
+        <button id="z" onclick="letterInput(this)">Z</button>
+        <button id="x" onclick="letterInput(this)">X</button>
+        <button id="c" onclick="letterInput(this)">C</button>
+        <button id="v" onclick="letterInput(this)">V</button>
+        <button id="b" onclick="letterInput(this)">B</button>
+        <button id="n" onclick="letterInput(this)">N</button>
+        <button id="m" onclick="letterInput(this)">M</button>
+    </div>
 `
 gameDiv.appendChild(keyboard);
